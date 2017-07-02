6677f87b8584345d8ddb0e14d86b14b1d7e946c8
Emir Mamashov <emir.mamashov@gmail.com>
2017-07-02 15:40:06 +0600

    check required for name

--- routes/product.js
+++ routes/product.js
@@ -82,10 +82,19 @@ module.exports = (app, db) => {
         console.log(req.body);
         uploadFile(req, res).then(
             (file) => {
-                //console.log(file);
-                //filters.input.validate(productForm);
+                let name = req.body.name;
+                if (!name) return res.status(200).json({
+                    success: false,
+                    status: 'yellow',
+                    message: 'Введите наименование товара',
+                    data: {
+                        code: 403,
+                        message: 'name is null'
+                    }
+                });
+
                 let newProduct = new db.Product({
-                    name: req.body.name || '',
+                    name: name,
                     htmlH1: req.body.htmlH1 || '',
                     htmlTitle: req.body.htmlTitle || '',
                     metaDescription: req.body.metaDescription || '',
@@ -201,6 +210,16 @@ module.exports = (app, db) => {
         uploadFile(req, res).then(
             (file) => {
                 console.log(req.body);
+                let name = req.body.name;
+                if (!name) return res.status(200).json({
+                    success: false,
+                    status: 'yellow',
+                    message: 'Введите наименование товара',
+                    data: {
+                        code: 403,
+                        message: 'name is null'
+                    }
+                });
                 req.body.image = file ? '/uploads' + file.path.replace(config.UPLOAD_DIR, '') : '';
                 db.Product.findById(req.body._id).then(
                     (product) => {
@@ -280,15 +299,94 @@ module.exports = (app, db) => {
             }
         ).catch(
             (err) => {
-                res.status(200).json({
+                console.log(err);
+                console.log(req.body);
+                let name = req.body.name;
+                if (!name) return res.status(200).json({
                     success: false,
-                    message: 'Что то пошло не так',
-                    status: 'red',
+                    status: 'yellow',
+                    message: 'Введите наименование товара',
                     data: {
-                        code: 500,
-                        message: err
+                        code: 403,
+                        message: 'name is null'
                     }
                 });
+                req.body.image = file ? '/uploads' + file.path.replace(config.UPLOAD_DIR, '') : '';
+                db.Product.findById(req.body._id).then(
+                    (product) => {
+                        console.log(product);
+                        if (!product) {
+                            return res.status(200).json({
+                                success: false,
+                                status: 'yellow',
+                                message: 'Товар не найдено',
+                                data: {
+                                    code: 404,
+                                    message: 'product not found'
+                                }
+                            });
+                        }
+
+                        product.name = req.body.name;
+                        product.htmlH1 = req.body.htmlH1;
+                        product.htmlTitle = req.body.htmlTitle;
+                        product.metaDescription = req.body.metaDescription;
+                        product.metaKeywords = req.body.metaKeywords;
+                        product.description = req.body.description;
+                        product.tegs = req.body.tegs;
+                        product.phone = req.body.phone;
+                        product.price = req.body.price;
+                        product.priceStock = req.body.priceStock;
+                        product.seoUrl = req.body.seoUrl;
+                        product.promoStickers = req.body.promoStickers;
+                        product.image = file ? '/uploads' + file.path.replace(config.UPLOAD_DIR, '') : '';
+                        product.producer = req.body.producer;
+                        product.category = req.body.category;
+                        product.categories = req.body.categories;
+
+                        product.save().then(
+                            () => {
+                                res.status(200).json({
+                                    success: true,
+                                    status: 'green',
+                                    message: 'Сохранено',
+                                    data: {
+                                        code: 200,
+                                        message: 'Update success!',
+                                        data: {
+                                            product: product
+                                        }
+                                    }
+                                });
+                            }
+                        ).catch(
+                            (err) => {
+                                console.log(err);
+                                res.status(200).json({
+                                    success: false,
+                                    status: 'red',
+                                    message: 'Что то пошло не так',
+                                    data: {
+                                        code: 500,
+                                        message: err
+                                    }
+                                });
+                            }
+                        );
+                    }
+                ).catch(
+                    (err) => {
+                        res.status(200).json({
+                            success: false,
+                            message: 'Что то пошло не так',
+                            status: 'red',
+                            data: {
+                                code: 500,
+                                message: err
+                            }
+                        });
+                    }
+                );
             }
         );
     });
